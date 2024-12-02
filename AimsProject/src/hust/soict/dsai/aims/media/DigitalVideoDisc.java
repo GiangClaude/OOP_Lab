4bf9e25f1e684052143999f922719c902abebf40
GiangClaude <165342359+GiangClaude@users.noreply.github.com>
2024-12-02 07:52:14 +0700

    update

--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,46 +1,21 @@
-package hust.soict.dsai.aims.disc;
+package hust.soict.dsai.aims.media;
 
 
-public class DigitalVideoDisc {
+public class DigitalVideoDisc extends Media {
 	private static int nbDigitalVideoDisc;
-	private int id;
-	private String title;
-	private String category;
 	private String director;
 	private int length;
-	private float cost;
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
 	public void setDirector(String director) {
 		this.director = director;
 	}
 	public void setLength(int length) {
 		this.length = length;
 	}
-	public void setCost(float cost) {
-		this.cost = cost;
-	}
-	public int getID() {
-		return id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
 	public String getDirector() {
 		return director;
 	}
 	public int getLength() {
 		return length;
-	}
-	public float getCost() {
-		return cost;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
